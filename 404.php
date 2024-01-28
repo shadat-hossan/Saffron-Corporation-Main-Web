c4d4023460b43b6ffa7bb921be1732d52d0f460e
shadat-hossan <shadathossan3500@gmail.com>
2024-01-28 10:50:27 +0600

    added 404 and some chinge style.css

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php include('header.php');?>
+
+<div class="tx_404_area">
+    <div class="tx_404_box">
+        <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script>
+        <dotlottie-player src="assets/json/12.json" background="transparent" speed="1" style="width: 200px; height: 200px" direction="1" playMode="normal" loop  autoplay></dotlottie-player>
+        <h4>Oops... page not found!</h4>
+        <a class="red-button" href="index.php">Back to the Home page</a>
+    </div>
+</div>
+
+<?php include('footer.php');?>
